e98098b94e7bdf022065e015c16668d015e27435
Sushil Khatri <139630872+SuseelKc@users.noreply.github.com>
2023-12-11 23:43:30 +0545

    welcome page

---
+++ resources/views/.blade.php
@@ -0,0 +1,15 @@
+<div class="row md-6">
+    @if (Route::has('login'))
+        <div>
+            @auth
+                <a href="{{ url('/dashboard') }}">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif
+</div>
