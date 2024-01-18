791c7f3fd07dafb8ef266cdccb47354e5679996f
SuseelKc <sushil.np.khatri@gmail.com>
2024-01-18 23:53:16 +0545

    sidebar payment

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -80,6 +80,18 @@
                 </li> 
 
 
+                <li class="nav-item m-1">
+                    <a href="
+                    {{-- {{ route('dashboard') }} --}}
+                    "
+                        class="nav-link {{ request()->is('shop*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-cash-register  m-1 p-1"></i>
+                        <p>
+                            Payments
+                        </p>
+                    </a>
+                </li> 
+
             
             </ul>
         </nav>
