bf698a1107a9643c1af29345c153d66cb6f84fc2
SuseelKc <sushil.np.khatri@gmail.com>
2024-01-29 23:28:13 +0545

    modal

--- resources/views/admin/equipments/index.blade.php
+++ resources/views/admin/equipments/index.blade.php
@@ -75,7 +75,7 @@
 </div>
 <!--  -->
 <!-- Modal -->
-{{-- <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
     <div class="modal-header">
@@ -95,7 +95,7 @@
             </div>
     </div>
   </div>
-</div> --}}
+</div>
 <!--  -->
 {{-- <script>
     $(document).ready(function () {
