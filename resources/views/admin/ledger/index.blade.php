8de4934a457e456cf448eb6bc3fa0768c92d5984
SuseelKc <sushil.np.khatri@gmail.com>
2024-02-16 23:22:04 +0545

    search of particular member ledger

--- resources/views/admin/ledger/index.blade.php
+++ resources/views/admin/ledger/index.blade.php
@@ -1,18 +1,18 @@
 @extends('admin.admin')
-@section('title','Payment Ledger')
+@section('title', 'Payment Ledger')
 @section('content')
 {{--  --}}
 <div class="content">
     <section class="content-header">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-12">
-                        <ol class="breadcrumb float-right">
-                            <li class="breadcrumb-item active">Gym Clients</li>
-                        </ol>
-                    </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ol class="breadcrumb float-right">
+                        <li class="breadcrumb-item active">Gym Clients</li>
+                    </ol>
                 </div>
             </div>
+        </div>
     </section>
 
     <section class="content">
@@ -21,66 +21,69 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="ml-2 mt-5 mb-3">
-                            <label>Members </label>
+                            <label>Members</label>
                             <select name="members" id="members">
                                 @foreach($members as $singleMember)
                                     <option value="{{ $singleMember['id'] }}">{{ $singleMember['name'] }}</option>
                                 @endforeach
                             </select>
-                            <a href="#" id="searchLink" class="btn btn-primary"><i class='fas fa-search'></i></a>  
+                            <a href="#" class="btn btn-primary" id="searchBtn"><i class='fas fa-search'></i></a>
                         </div>
-                           
-                            {{-- <div class="">                             
-                                <a href="
-                                {{route('member.create')}}
-                                "
-                                    class="btn btn-primary px-4 m-2 float-right">Find</a>  
-                            </div> --}}
-                            <div class="card-body table-responsive p-2">
-                                <table class="datatable table">
-                                    <thead>
-                                        <tr>
-                                            <th>S.No</th>
-                                            <th>Date</th>
-                                            <th>Debit</th>
-                                            <th>Credit</th>                                         
-                                            <th>Balance</th>
-                                            <th>Package</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($ledger as $ledger)
-                                        <tr>
-                                            <td>{{$loop->iteration}}</td>
-                                            <td>{{$ledger->date}}</td>                                          
-                                            <td>{{$ledger->debit}}</td>
-                                            <td>{{$ledger->credit}}</td>
-                                            <td>{{$ledger->balance}}</td>
-                                            <td>{{$ledger->member_id}}</td>
-                                            <td>{{$ledger->gym_id}}</td>
-                                            <td></td>
-                                            
+                        
 
-                                        </tr>
-                                        @endforeach    
-                                    </tbody>
-                                </table>
-                            </div>
+                        <div class="card-body table-responsive p-2">
+                            <table class="datatable table">
+                                <thead>
+                                    <tr>
+                                        <th>S.No</th>
+                                        <th>Date</th>
+                                        <th>Debit</th>
+                                        <th>Credit</th>
+                                        <th>Balance</th>
+                                        <th>Member</th>
+                                        <th>Package</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($ledger as $ledger)
+                                    <tr>
+                                        <td>{{$loop->iteration}}</td>
+                                        <td>{{$ledger->date}}</td>
+                                        <td>{{$ledger->debit}}</td>
+                                        <td>{{$ledger->credit}}</td>
+                                        <td>{{$ledger->balance}}</td>
+                                        <td>{{$ledger->member_id}}</td>
+                                        <td>{{$ledger->gym_id}}</td>
+                                        <td></td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 </div>
 
 <!--  -->
+    <script>
+        $(document).ready(function() {
+            $('#members').on('change', function(){
+                var memberId = $(this).val();
+                console.log("Selected member ID:", memberId); // Debugging line
+                
+                // Construct the URL dynamically with the selected member's ID
+                var url = "{{ route('ledger.search', ':id') }}";
+                url = url.replace(':id', memberId);
+                console.log("Dynamic URL:", url); // Debugging line
+                
+                // Update the href attribute of the search button with the constructed URL
+                $('#searchBtn').attr('href', url);
+            });
+        });
+    </script>
 @endsection
-
-<script>
-    document.getElementById('members').addEventListener('change', function() {
-        var memberId = this.value; // Get the selected member ID
-        var searchLink = document.getElementById('searchLink');
-        searchLink.href = "http://127.0.0.1:8000/ledger#" + memberId;
-    });
-</script>
---
+++ resources/views/admin/ledger/search.blade.php
@@ -0,0 +1,83 @@
+@extends('admin.admin')
+@section('title','Payment Ledger')
+@section('content')
+{{--  --}}
+<div class="content">
+    <section class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <ol class="breadcrumb float-right">
+                            <li class="breadcrumb-item active">Gym Clients</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="ml-2 mt-5 mb-3">
+                            <label>Members </label>
+                            <select name="members" id="members">
+                                @foreach($members as $singleMember)
+                                    <option value="{{ $singleMember['id'] }}">{{ $singleMember['name'] }}</option>
+                                @endforeach
+                            </select>
+                            <a href="#" id="searchLink" class="btn btn-primary"><i class='fas fa-search'></i></a>  
+                        </div>
+                           
+                            {{-- <div class="">                             
+                                <a href="
+                                {{route('member.create')}}
+                                "
+                                    class="btn btn-primary px-4 m-2 float-right">Find</a>  
+                            </div> --}}
+                            <div class="card-body table-responsive p-2">
+                                <table class="datatable table">
+                                    <thead>
+                                        <tr>
+                                            <th>S.No</th>
+                                            <th>Date</th>
+                                            <th>Debit</th>
+                                            <th>Credit</th>                                         
+                                            <th>Balance</th>                                                                               
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($ledger as $ledger)
+                                        <tr>
+                                            <td>{{$loop->iteration}}</td>
+                                            <td>{{$ledger->date}}</td>                                          
+                                            <td>{{$ledger->debit}}</td>
+                                            <td>{{$ledger->credit}}</td>
+                                            <td>{{$ledger->balance}}</td>                                          
+                                            <td></td>
+                                            
+
+                                        </tr>
+                                        @endforeach    
+                                    </tbody>
+                                </table>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<!--  -->
+@endsection
+
+<script>
+    document.getElementById('members').addEventListener('change', function() {
+        var memberId = this.value; // Get the selected member ID
+        var searchLink = document.getElementById('searchLink');
+        searchLink.href = "http://127.0.0.1:8000/ledger#" + memberId;
+    });
+</script>
