c295619d12aed3a908854b7606517676e650737a
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-08-10 21:02:59 +0545

    fixed the sidebar active issue

--- routes/web/staff.php
+++ routes/web/staff.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\Web\StaffController;
 
 
 
-Route::get('/staffs',[StaffController::class,'index'])->name('staffs.index');
+// Route::get('/staffs',[StaffController::class,'index'])->name('staffs.index');
 Route::get('/staffs/add',[StaffController::class,'create'])->name('staffs.create');
 Route::post('/staffs/store',[StaffController::class,'store'])->name('staffs.store');
 Route::get('/staffs/edit/{id}',[StaffController::class,'edit'])->name('staffs.edit');
 Route::patch('/staffs/update/{id}',[StaffController::class,'update'])->name('staffs.update');
 Route::get('/staffs/{id}',[StaffController::class,'delete'])->name('staffs.delete');
 
-Route::get('/staff',[StaffController::class,'staffIndex'])->name('staffs.staffindex');
+Route::get('/staffs',[StaffController::class,'staffIndex'])->name('staffs.staffindex');
 Route::post('/allStaff',[StaffController::class,'listStaff'])->name('staffs.list');
 Route::post('/staff/save',[StaffController::class,'saveStaff'])->name('staffs.save');
 Route::get('/staff/{id}/edit', [StaffController::class, 'getDataForStaffEdit'])->name('staffs.getDataForEdit');
